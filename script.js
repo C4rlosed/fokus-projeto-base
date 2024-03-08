805b6ae3ec8e0cf91be70503e1f6c8c8f7757963
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2024-03-08 14:48:56 -0300

    Att add e remover class Active

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button');
 //TEMPORIZADOR
 const displayTempo = document.querySelector('#timer');
 //IMAGEM
@@ -21,17 +22,24 @@ const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+        
+    });
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
 
